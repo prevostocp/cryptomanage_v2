8c41d01b66f3854356d779de20de65673f701693
David Prevosto <david_zeppelin@hotmail.com>
2024-07-19 19:55:16 -0300

    Creacion de componentes y modificaciones de campos de tabla

--- cryptomanage_api/settings.py
+++ cryptomanage_api/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'cryptomanage_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'cryptomanage_V2',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'admin',
-    #     'HOST': 'localhost',
-    #     'PORT': '5433'
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cryptomanageV2',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5433'
+    }
 }
 
 

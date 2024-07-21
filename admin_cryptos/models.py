63462b1b3637d4be65e7a8432311dba434ab5657
David Prevosto <david_zeppelin@hotmail.com>
2024-07-20 21:52:16 -0300

    CRMA-00004, CRMA-00005, CRMA-00006

--- admin_cryptos/models.py
+++ admin_cryptos/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Coin(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=False, unique=True, verbose_name="Name")
     description = models.TextField(blank=True)
     image = models.CharField(max_length=200, blank=True)
     webSite = models.CharField(max_length=200, blank=True)
@@ -12,3 +12,53 @@ class Coin(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        db_table = "coins"
+        verbose_name = "Coin"
+        verbose_name_plural = "Coins"
+        ordering = ["id"]
+
+class Blockchain(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True, verbose_name="Name")
+    description = models.TextField(blank=True, verbose_name="Description")
+    image = models.CharField(max_length=200, blank=True)
+    webSite = models.CharField(max_length=200, blank=True)
+    blockScan = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = "blockchains"
+        verbose_name = "Blockchain"
+        verbose_name_plural = "Blockchains"
+        ordering = ["id"]
+
+class Stanking(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True, verbose_name="Name")
+    description = models.TextField(blank=True, verbose_name="Description")
+    image = models.CharField(max_length=200, blank=True)
+    webSite = models.CharField(max_length=200, blank=True)
+    blockchain = models.ForeignKey(Blockchain, on_delete=models.CASCADE, null=False, blank=False)
+    type = models.SmallIntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = "stakings"
+        verbose_name = "Staking"
+        verbose_name_plural = "Stakings"
+        ordering = ["id"]
+
+class StakingDetail(models.Model):
+    staking = models.ForeignKey(Stanking, on_delete=models.CASCADE, null=False, blank=False, related_name="items" ) # model_set
+    quantity = models.FloatField(default=0)
+    price = models.FloatField(default=0)
+
+    class Meta:
+        db_table = "stakingDetails"
+        verbose_name = "StakingDetail"
+        verbose_name_plural = "StakingDetails"
+        ordering = ["id"]
